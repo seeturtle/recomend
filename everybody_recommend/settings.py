aac7c8522690e41929cdaba4dec30a3fc5276385
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2019-04-07 22:11:28 +0900

    update setting

--- everybody_recommend/settings.py
+++ everybody_recommend/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = os.getenv('LANGUAGE_CODE')
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.getenv('TIME_ZONE')
 
 USE_I18N = True
 
