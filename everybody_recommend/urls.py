8b7180d998ddb3b39ff615329668f590e9c76352
skyloken <risora1234@gmail.com>
2019-04-15 02:39:31 +0900

    some fix

--- everybody_recommend/urls.py
+++ everybody_recommend/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
