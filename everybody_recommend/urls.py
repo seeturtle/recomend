45de10780342ab647197ea87124964caca432d35
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2019-05-06 22:28:39 +0900

    update setting (change directory architecture)

--- everybody_recommend/urls.py
+++ everybody_recommend/urls.py
@@ -21,6 +21,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homes.urls')),
     path('polls/', include('polls.urls')),
     path('users/', include('users.urls')),
 ]
