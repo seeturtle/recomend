3bbadda2f1b6a80be3e8ed147191cf6faf20beb8
Kenta Tanigaki <risora1234@gmail.com>
2019-05-14 00:17:09 +0900

    UIKitの導入，マイページの調整

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import CustomUserCreationForm
+from .models import User
 
 
 class SignUpView(generic.CreateView):
@@ -11,5 +12,7 @@ class SignUpView(generic.CreateView):
     template_name = 'users/signup.html'
 
 
-def mypage(request):
-    return render(request, 'users/mypage.html')
+def mypage(request, username):
+    user = User.objects.get(username=username)
+    context = {'myuser': user}
+    return render(request, 'users/mypage.html', context)
