59ea525a4782c771377a0d8c4ac14e07a4330b74
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2019-05-27 00:14:24 +0900

    コメント機能追加

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,5 @@ app_name = 'polls'
 urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
     path('post', views.post, name='post'),
+    path('comment', views.comment, name='comment'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.shortcuts import render,  redirect, get_object_or_404
+from .models import *
 from .forms import *
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+
+    comments = Comment.objects.filter(
+        target_question_id=question_id, reply_to_id__isnull=True)
+
+    commentForm = CommentForm
+    return render(request, 'polls/detail.html', {'question': question, 'comments': comments, 'commentForm': commentForm})
 
 
 def post(request):
@@ -30,3 +35,25 @@ def post(request):
         questionForm = QuestionForm
 
     return render(request, 'polls/post.html', {'form': questionForm, 'tag': tagForm, 'm': m})
+
+
+def comment(request):
+
+    if request.method == "POST":
+        commentForm = CommentForm(request.POST)
+
+        if commentForm.is_valid():
+
+            comment = commentForm.save(commit=False)
+            comment.user = request.user
+            comment.target_question_id = request.POST["question_id"]
+
+            # コメントに対するコメント時
+            if request.POST["reply_to_id"]:
+                comment.reply_to_id = request.POST["reply_to_id"]
+            comment.save()
+
+        # TODO
+        # target_recommend_id時のコメント
+
+    return redirect('polls:detail', question_id=request.POST["question_id"])
