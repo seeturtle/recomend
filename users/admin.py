b6648b37209259bdf775960bcee1e5a6e331883f
Kenta Tanigaki <risora1234@gmail.com>
2019-05-26 22:24:18 +0900

    マイページの作成

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
 
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('icon', 'username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'profile')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
