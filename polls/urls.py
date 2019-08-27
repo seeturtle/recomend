79e4d2cfdcf0f8d18ffd4fce5731129a18f7b9c3
Kenta Tanigaki <risora1234@gmail.com>
2019-08-28 00:38:29 +0900

    ベストレコメンド機能を実装

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ app_name = 'polls'
 
 urlpatterns = [
     path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/recommends/<int:recommend_id>/best', views.set_best_recommend, name='set_best_recommend'),
     path('post', views.post, name='post'),
     path('comment', views.comment, name='comment'),
 ]
