354e9f119bc311ac79bdd38147d98e3b7f7497c5
Kenta Tanigaki <risora1234@gmail.com>
2019-09-02 22:17:29 +0900

    プロフィール編集機能を実装

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 
     path('users/<str:username>/', views.mypage, name='mypage'),
+    path('users/edit', views.UserChangeView.as_view(), name='edit_profile'),
 
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 
 
@@ -16,3 +18,13 @@ def mypage(request, username):
     user = User.objects.get(username=username)
     context = {'myuser': user}
     return render(request, 'users/mypage.html', context)
+
+
+class UserChangeView(LoginRequiredMixin, generic.UpdateView):
+    model = get_user_model()
+    form_class = CustomUserChangeForm
+    template_name = 'users/edit_profile.html'
+    success_url = reverse_lazy('edit_profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
